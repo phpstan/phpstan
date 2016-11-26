f9c5cfa3cecd9189fb09b31b021ee32aa6ed7fb1
Ondrej Mirtes <ondrej@mirtes.cz>
2016-11-26 15:20:59 +0100

    CallableType accepts obects of type Closure

--- src/Type/CallableType.php
+++ src/Type/CallableType.php
@@ -5,7 +5,61 @@ namespace PHPStan\Type;
 class CallableType implements Type
 {
 
-	use JustNullableTypeTrait;
+	/** @var bool */
+	private $nullable;
+
+	public function __construct(bool $nullable)
+	{
+		$this->nullable = $nullable;
+	}
+
+	/**
+	 * @return string|null
+	 */
+	public function getClass()
+	{
+		return null;
+	}
+
+	public function isNullable(): bool
+	{
+		return $this->nullable;
+	}
+
+	public function combineWith(Type $otherType): Type
+	{
+		if ($otherType instanceof self) {
+			return new self($this->isNullable() || $otherType->isNullable());
+		}
+
+		if ($otherType instanceof NullType) {
+			return $this->makeNullable();
+		}
+
+		return new MixedType($this->isNullable() || $otherType->isNullable());
+	}
+
+	public function makeNullable(): Type
+	{
+		return new self(true);
+	}
+
+	public function accepts(Type $type): bool
+	{
+		if ($type instanceof self) {
+			return true;
+		}
+
+		if ($this->isNullable() && $type instanceof NullType) {
+			return true;
+		}
+
+		if ($type->getClass() === 'Closure') {
+			return true;
+		}
+
+		return $type instanceof MixedType;
+	}
 
 	public function describe(): string
 	{
