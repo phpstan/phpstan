1284a5170cdf296a39881f7a632dfd60ebebfa22
Ondrej Mirtes <ondrej@mirtes.cz>
2016-12-11 13:45:02 +0100

    AnalyserIntegrationTest - DRYed running analyse()

--- tests/PHPStan/Analyser/AnalyserIntegrationTest.php
+++ tests/PHPStan/Analyser/AnalyserIntegrationTest.php
@@ -7,38 +7,45 @@ class AnalyserIntegrationTest extends \PHPStan\TestCase
 
 	public function testUndefinedVariableFromAssignErrorHasLine()
 	{
-		$file = __DIR__ . '/data/undefined-variable-assign.php';
-		$analyser = $this->getContainer()->getByType(Analyser::class);
-		$errors = $analyser->analyse([$file]);
+		$errors = $this->runAnalyse(__DIR__ . '/data/undefined-variable-assign.php');
 		$this->assertCount(1, $errors);
 		$error = $errors[0];
 		$this->assertSame('Undefined variable: $bar', $error->getMessage());
-		$this->assertSame($file, $error->getFile());
 		$this->assertSame(3, $error->getLine());
 	}
 
 	public function testMissingPropertyAndMethod()
 	{
-		$file = __DIR__ . '/../../notAutoloaded/Foo.php';
-		$analyser = $this->getContainer()->getByType(Analyser::class);
-		$errors = $analyser->analyse([$file]);
+		$errors = $this->runAnalyse(__DIR__ . '/../../notAutoloaded/Foo.php');
 		$this->assertCount(1, $errors);
 		$error = $errors[0];
 		$this->assertSame('Property $fooProperty was not found in reflection of class PHPStan\Tests\Foo - probably the wrong version of class is autoloaded.', $error->getMessage());
-		$this->assertSame($file, $error->getFile());
 		$this->assertNull($error->getLine());
 	}
 
 	public function testErrorAboutMisconfiguredAutoloader()
 	{
-		$file = __DIR__ . '/../../notAutoloaded/Bar.php';
-		$analyser = $this->getContainer()->getByType(Analyser::class);
-		$errors = $analyser->analyse([$file]);
+		$errors = $this->runAnalyse(__DIR__ . '/../../notAutoloaded/Bar.php');
 		$this->assertCount(1, $errors);
 		$error = $errors[0];
 		$this->assertSame('Class PHPStan\Tests\Bar was not found while trying to analyse it - autoloading is not probably configured properly.', $error->getMessage());
-		$this->assertSame($file, $error->getFile());
 		$this->assertNull($error->getLine());
+	}
+
+	/**
+	 * @param string $file
+	 * @return \PHPStan\Analyser\Error[]
+	 */
+	private function runAnalyse(string $file): array
+	{
+		/** @var \PHPStan\Analyser\Analyser $analyser */
+		$analyser = $this->getContainer()->getByType(Analyser::class);
+		$errors = $analyser->analyse([$file]);
+		foreach ($errors as $error) {
+			$this->assertSame($file, $error->getFile());
+		}
+
+		return $errors;
 	}
 
 }
