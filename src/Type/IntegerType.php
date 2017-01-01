d93c77ecd9f24a9a826203325e36f7b12da2ada3
Josef Kriz <pepakriz@gmail.com>
2017-01-01 12:48:09 +0100

    IntegerType: float after combineWith integer

--- src/Type/IntegerType.php
+++ src/Type/IntegerType.php
@@ -7,6 +7,23 @@ class IntegerType implements Type
 
 	use JustNullableTypeTrait;
 
+	public function combineWith(Type $otherType): Type
+	{
+		if ($otherType instanceof self) {
+			return new self($this->isNullable() || $otherType->isNullable());
+		}
+
+		if ($otherType instanceof FloatType) {
+			return new FloatType($this->isNullable() || $otherType->isNullable());
+		}
+
+		if ($otherType instanceof NullType) {
+			return $this->makeNullable();
+		}
+
+		return new MixedType();
+	}
+
 	public function describe(): string
 	{
 		return 'int' . ($this->nullable ? '|null' : '');
