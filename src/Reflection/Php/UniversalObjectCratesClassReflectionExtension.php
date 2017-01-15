d48e3718e343d97da14c97d4d62644f5f0131bf5
Ondrej Mirtes <ondrej@mirtes.cz>
2017-01-15 18:02:28 +0100

    Universal object crates extension - do not crash on nonexistent class, skip it

--- src/Reflection/Php/UniversalObjectCratesClassReflectionExtension.php
+++ src/Reflection/Php/UniversalObjectCratesClassReflectionExtension.php
@@ -4,10 +4,13 @@ namespace PHPStan\Reflection\Php;
 
 use PHPStan\Reflection\ClassReflection;
 use PHPStan\Reflection\PropertyReflection;
+use PHPStan\Type\ClassTypeHelperTrait;
 
 class UniversalObjectCratesClassReflectionExtension
 	implements \PHPStan\Reflection\PropertiesClassReflectionExtension
 {
+
+	use ClassTypeHelperTrait;
 
 	/** @var string[] */
 	private $classes;
@@ -17,7 +20,9 @@ class UniversalObjectCratesClassReflectionExtension
 	 */
 	public function __construct(array $classes)
 	{
-		$this->classes = $classes;
+		$this->classes = array_values(array_filter($classes, function (string $class): bool {
+			return self::exists($class);
+		}));
 	}
 
 	public function hasProperty(ClassReflection $classReflection, string $propertyName): bool
---
+++ tests/PHPStan/Reflection/Php/UniversalObjectCratesClassReflectionExtensionTest.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Reflection\Php;
+
+use PHPStan\Broker\Broker;
+
+class UniversalObjectCratesClassReflectionExtensionTest extends \PHPStan\TestCase
+{
+
+	public function testNonexistentClass()
+	{
+		$broker = $this->getContainer()->getByType(Broker::class);
+		$extension = new UniversalObjectCratesClassReflectionExtension([
+			'NonexistentClass',
+			'stdClass',
+		]);
+		$this->assertTrue($extension->hasProperty($broker->getClass(\stdClass::class), 'foo'));
+	}
+
+}
