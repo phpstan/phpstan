40b0c17ca2021bfc3e475a08b16b4f51f948ab30
Ondrej Mirtes <ondrej@mirtes.cz>
2017-01-21 16:17:47 +0100

    Processing switch condition

--- src/Analyser/NodeScopeResolver.php
+++ src/Analyser/NodeScopeResolver.php
@@ -383,6 +383,7 @@ class NodeScopeResolver
 
 			return;
 		} elseif ($node instanceof Switch_) {
+			$this->processNode($node->cond, $scope, $nodeCallback);
 			$scope = $this->lookForAssigns($scope, $node->cond);
 			$switchScope = $scope;
 			$switchConditionIsTrue = $node->cond instanceof Expr\ConstFetch && strtolower((string) $node->cond->name) === 'true';
