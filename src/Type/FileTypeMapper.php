67f82c57bf41bca40c3c8c48cb011bcf2c92187c
吕海涛 <git@lvht.net>
2017-03-12 13:44:45 +0800

    fix phar:// path error

--- src/Type/FileTypeMapper.php
+++ src/Type/FileTypeMapper.php
@@ -36,7 +36,7 @@ class FileTypeMapper
 
     public function getTypeMap(string $fileName): array
     {
-        $cacheKey = sprintf('%s-%d-v24-%d', $fileName, filemtime($fileName), $this->enableUnionTypes ? 1 : 0);
+        $cacheKey = sprintf('%s-%d-v24-%d', str_replace('phar://', '', $fileName), filemtime($fileName), $this->enableUnionTypes ? 1 : 0);
         if (isset($this->memoryCache[$cacheKey])) {
             return $this->memoryCache[$cacheKey];
         }
