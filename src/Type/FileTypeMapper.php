ca2d37b6ee6c07132b743c7205962a20b610a33d
Richard Quadling <Richard.Quadling@DigiTickets.co.uk>
2017-03-20 15:36:46 +0000

    Support docblock method signatures without parenthesise
    Fix https://github.com/phpstan/phpstan/issues/193
    
    There are 2 signatures that cannot be parsed appropriately without some
    more thought:
    
     * @method IsThisTheMethodNameOrReturnType IsThisTheMethodNameOrTheFirstWordOfTheDescription IsThisTheFirstOrSecondWordOfTheDescription
     * @method static IsThisTheStaticMethodNameOrReturnType IsThisTheStaticMethodNameOrTheFirstWordOfTheDescription IsThisTheFirstOrSecondWordOfTheDescription

--- src/Type/FileTypeMapper.php
+++ src/Type/FileTypeMapper.php
@@ -37,7 +37,7 @@ class FileTypeMapper
 
 	public function getTypeMap(string $fileName): array
 	{
-		$cacheKey = sprintf('%s-%d-v25-%d', $fileName, filemtime($fileName), $this->enableUnionTypes ? 1 : 0);
+		$cacheKey = sprintf('%s-%d-v26-%d', $fileName, filemtime($fileName), $this->enableUnionTypes ? 1 : 0);
 		if (isset($this->memoryCache[$cacheKey])) {
 			return $this->memoryCache[$cacheKey];
 		}
@@ -63,7 +63,7 @@ class FileTypeMapper
 			'#@var\s+\$[a-zA-Z0-9_]+\s+' . self::TYPE_PATTERN . '#',
 			'#@return\s+' . self::TYPE_PATTERN . '#',
 			'#@property(?:-read)?\s+' . self::TYPE_PATTERN . '\s+\$[a-zA-Z0-9_]+#',
-			'#@method\s+(?:static\s+)?' . self::TYPE_PATTERN . '\s*?[a-zA-Z0-9_]+\(.*\)#',
+			'#@method\s+(?:static\s+)?' . self::TYPE_PATTERN . '\s*?[a-zA-Z0-9_]+(?:\(.*\))?#',
 		];
 
 		/** @var \PhpParser\Node\Stmt\ClassLike|null $lastClass */
