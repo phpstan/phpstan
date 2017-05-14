96f9c506953e85f63084749aeb086a4f070acbad
Ondrej Mirtes <ondrej@mirtes.cz>
2017-05-14 16:51:07 +0200

    Testing specific issue of calling methods on null

---
+++ tests/PHPStan/Rules/Methods/data/order.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace CallMethodsNullIssue;
+
+class Foo
+{
+
+	/**
+	 * @return int|null
+	 */
+	public function lorem()
+	{
+
+	}
+
+	/**
+	 * @return int|null
+	 */
+	public function ipsum()
+	{
+
+	}
+
+}
+
+class Bar
+{
+
+	/**
+	 * @return int|null
+	 */
+	public function lorem()
+	{
+
+	}
+
+	/**
+	 * @return int|null
+	 */
+	public function ipsum()
+	{
+
+	}
+
+}
+
+class Baz
+{
+
+	/**
+	 * @return Foo|null
+	 */
+	public function getFoo(): Foo
+	{
+
+	}
+
+	/**
+	 * @return Bar|null
+	 */
+	public function getBar(): Bar
+	{
+
+	}
+
+	public function process(): string
+	{
+		if ($this->getFoo() === null && $this->getBar() === null) {
+			return '';
+		}
+		if ($this->getFoo() !== null && $this->getBar() === null) {
+			return '';
+		} elseif ($this->getBar() !== null && $this->getFoo() === null) {
+			return '';
+		}
+
+		$foo = $this->getFoo();
+		$bar = $this->getBar();
+		if ($bar->lorem() !== null && $foo->lorem() === null) {
+			return '';
+		} elseif ($bar->lorem() === null && $foo->lorem() !== null) {
+			return '';
+		} elseif ($bar->lorem() !== null && $foo->lorem() !== null) {
+			return $bar->lorem() > $foo->lorem() ? '' : '';
+		}
+		if ($foo->ipsum() < $bar->ipsum()) {
+			return '';
+		}
+
+		return '';
+	}
+
+}
