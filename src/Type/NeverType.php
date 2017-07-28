f9b10cacbdc820b91bc9741383c6a5059aa7a414
Jan Tvrdik <jan.tvrdik@gmail.com>
2017-07-28 15:17:22 +0200

    add NeverType

---
+++ src/Type/NeverType.php
@@ -0,0 +1,81 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Type;
+
+use PHPStan\TrinaryLogic;
+
+class NeverType implements CompoundType
+{
+
+	/**
+	 * @return string|null
+	 */
+	public function getClass()
+	{
+		return null;
+	}
+
+	/**
+	 * @return string[]
+	 */
+	public function getReferencedClasses(): array
+	{
+		return [];
+	}
+
+	public function combineWith(Type $otherType): Type
+	{
+		return $this;
+	}
+
+	public function accepts(Type $type): bool
+	{
+		return true;
+	}
+
+	public function describe(): string
+	{
+		return '*NEVER*';
+	}
+
+	public function canAccessProperties(): bool
+	{
+		return true;
+	}
+
+	public function canCallMethods(): bool
+	{
+		return true;
+	}
+
+	public function isDocumentableNatively(): bool
+	{
+		return true;
+	}
+
+	public function isIterable(): TrinaryLogic
+	{
+		return TrinaryLogic::createYes();
+	}
+
+	public function getIterableKeyType(): Type
+	{
+		return new NeverType();
+	}
+
+	public function getIterableValueType(): Type
+	{
+		return new NeverType();
+	}
+
+	public function isCallable(): TrinaryLogic
+	{
+		return TrinaryLogic::createYes();
+	}
+
+	public static function __set_state(array $properties): Type
+	{
+		return new self();
+	}
+
+}
