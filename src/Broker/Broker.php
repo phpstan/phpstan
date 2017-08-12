c450d0a56e3acd392d2c49eb1bc1b3aee3f96918
Ondrej Mirtes <ondrej@mirtes.cz>
2017-08-12 20:55:34 +0200

    Broker::hasClass microoptimization

--- src/Broker/Broker.php
+++ src/Broker/Broker.php
@@ -40,6 +40,9 @@ class Broker
 	/** @var null|self */
 	private static $instance;
 
+	/** @var bool[] */
+	private $hasClassCache;
+
 	/**
 	 * @param \PHPStan\Reflection\PropertiesClassReflectionExtension[] $propertiesClassReflectionExtensions
 	 * @param \PHPStan\Reflection\MethodsClassReflectionExtension[] $methodsClassReflectionExtensions
@@ -155,6 +158,10 @@ class Broker
 
 	public function hasClass(string $className): bool
 	{
+		if (isset($this->hasClassCache[$className])) {
+			return $this->hasClassCache[$className];
+		}
+
 		spl_autoload_register($autoloader = function (string $autoloadedClassName) use ($className) {
 			if ($autoloadedClassName !== $className) {
 				throw new \PHPStan\Broker\ClassAutoloadingException($autoloadedClassName);
@@ -162,7 +169,7 @@ class Broker
 		});
 
 		try {
-			return class_exists($className) || interface_exists($className) || trait_exists($className);
+			return $this->hasClassCache[$className] = class_exists($className) || interface_exists($className) || trait_exists($className);
 		} catch (\PHPStan\Broker\ClassAutoloadingException $e) {
 			throw $e;
 		} catch (\Throwable $t) {
