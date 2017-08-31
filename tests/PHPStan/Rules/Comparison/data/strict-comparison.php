45fa647316fcf831039da90fd4c8ea37e5d685b0
Jan Tvrdik <jan.tvrdik@gmail.com>
2017-08-31 21:07:27 +0200

    TypeSpecifier: fix negated identity comparison with true/false
    [closes #479]

--- tests/PHPStan/Rules/Comparison/data/strict-comparison.php
+++ tests/PHPStan/Rules/Comparison/data/strict-comparison.php
@@ -78,4 +78,21 @@ class Foo
 		}
 	}
 
+	public function returnsNullableString(): ?bool
+	{
+		return false;
+	}
+
+	public function doCheckNullableString(): int
+	{
+		if ($this->returnsNullableString() === true) {
+			return 1;
+		} else if ($this->returnsNullableString() === false) {
+			return 2;
+		} else if ($this->returnsNullableString() === null) {
+			return 3;
+		}
+		return 4;
+	}
+
 }
