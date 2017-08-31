2e5cd8b3cfd7b6e781ac3959b65aad3933dd4e06
Josef Kříž <pepakriz@gmail.com>
2017-08-31 15:45:52 +0200

    remove support for core php functions (will be back in next PR)

--- src/Type/Php/PhpCoreFunctionsReturnTypeExtension.php
+++ src/Type/Php/PhpCoreFunctionsReturnTypeExtension.php
@@ -7,75 +7,10 @@ use PhpParser\Node\Expr\FuncCall;
 use PHPStan\Analyser\Scope;
 use PHPStan\Reflection\FunctionReflection;
 use PHPStan\Type\ArrayType;
-use PHPStan\Type\NullType;
 use PHPStan\Type\Type;
-use PHPStan\Type\TypeCombinator;
-use PHPStan\Type\TypehintHelper;
 
 class PhpCoreFunctionsReturnTypeExtension implements \PHPStan\Type\DynamicFunctionReturnTypeExtension
 {
-
-	/** @var string[] */
-	private $coreFunctions = [
-		'zend_version' => 'string',
-		'func_num_args' => 'int',
-		'func_get_arg' => 'mixed|false',
-		'func_get_args' => 'array',
-		'strlen' => 'int',
-		'strcmp' => 'int',
-		'strncmp' => 'int',
-		'strcasecmp' => 'int',
-		'strncasecmp' => 'int',
-		'each' => 'array|false',
-		'error_reporting' => 'int',
-		'define' => 'bool',
-		'defined' => 'bool',
-		'get_class' => 'string|false',
-		'get_called_class' => 'string|false',
-		'get_parent_class' => 'string|false',
-		'method_exists' => 'bool',
-		'property_exists' => 'bool',
-		'trait_exists' => 'bool',
-		'class_exists' => 'bool',
-		'interface_exists' => 'bool',
-		'function_exists' => 'bool',
-		'class_alias' => 'bool',
-		'get_included_files' => 'string[]',
-		'get_required_files' => 'string[]',
-		'is_subclass_of' => 'bool',
-		'is_a' => 'bool',
-		'get_class_vars' => 'array',
-		'get_object_vars' => 'array',
-		'get_class_methods' => 'string[]',
-		'trigger_error' => 'bool',
-		'user_error' => 'bool',
-		'set_error_handler' => 'mixed',
-		'set_exception_handler' => 'callback',
-		'restore_exception_handler' => 'true',
-		'get_declared_classes' => 'string[]',
-		'get_declared_interfaces' => 'string[]',
-		'get_declared_traits' => 'string[]',
-		'get_defined_functions' => 'string[][]',
-		'get_defined_vars' => 'array',
-		'create_function' => 'string|false',
-		'get_resource_type' => 'string|false',
-		'get_loaded_extensions' => 'string[]',
-		'extension_loaded' => 'bool',
-		'get_extension_funcs' => 'string[]',
-		'get_defined_constants' => 'array',
-		'debug_backtrace' => 'array',
-		'error_clear_last' => 'void',
-		'debug_print_backtrace' => 'void',
-		'gc_collect_cycles' => 'int',
-		'gc_enabled' => 'bool',
-		'gc_enable' => 'void',
-		'gc_disable' => 'void',
-		'sapi_windows_cp_get' => 'int',
-		'sapi_windows_cp_set' => 'bool',
-		'sapi_windows_cp_conv' => 'string',
-		'sapi_windows_cp_is_utf8' => 'bool',
-		'is_iterable' => 'bool',
-	];
 
 	/** @var int[] */
 	private $arrayFunctionsThatCreateArrayBasedOnClosureReturnType = [
@@ -110,8 +45,7 @@ class PhpCoreFunctionsReturnTypeExtension implements \PHPStan\Type\DynamicFuncti
 	{
 		$functionName = strtolower($functionReflection->getName());
 
-		return isset($this->coreFunctions[$functionName])
-			|| isset($this->arrayFunctionsThatCreateArrayBasedOnClosureReturnType[$functionName])
+		return isset($this->arrayFunctionsThatCreateArrayBasedOnClosureReturnType[$functionName])
 			|| isset($this->arrayFunctionsThatDependOnClosureReturnType[$functionName])
 			|| isset($this->arrayFunctionsThatDependOnArgumentType[$functionName])
 			|| isset($this->arrayFunctionsThatCreateArrayBasedOnArgumentType[$functionName])
@@ -121,25 +55,6 @@ class PhpCoreFunctionsReturnTypeExtension implements \PHPStan\Type\DynamicFuncti
 	public function getTypeFromFunctionCall(FunctionReflection $functionReflection, FuncCall $functionCall, Scope $scope): Type
 	{
 		$functionName = strtolower($functionReflection->getName());
-
-		if (isset($this->coreFunctions[$functionName])) {
-			/** @var \PHPStan\Type\Type[] $types */
-			$types = [];
-
-			foreach (explode('|', $this->coreFunctions[$functionName]) as $typePart) {
-				$typePart = trim($typePart);
-				if ($typePart === '') {
-					continue;
-				}
-				if (substr($typePart, 0, 1) === '?') {
-					$typePart = substr($typePart, 1);
-					$types[] = new NullType();
-				}
-				$types[] = TypehintHelper::getTypeObjectFromTypehint($typePart);
-			}
-
-			return TypeCombinator::combine(...$types);
-		}
 
 		if (isset($this->arrayFunctionsThatCreateArrayBasedOnClosureReturnType[$functionName])) {
 			if (!isset($functionCall->args[$this->arrayFunctionsThatCreateArrayBasedOnClosureReturnType[$functionName]])) {
