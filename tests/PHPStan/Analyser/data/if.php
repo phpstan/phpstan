phpstan/phpstan

commit 22488d5aa657830ccbb72bcd666b7b6f55b5257a

Simplification of lookForAssignsInBranches

src/Analyser/Scope.php
@@ -1320,22 +1320,25 @@ class Scope
 		);
 	}
 
-	public function intersectVariables(Scope $otherScope, bool $addMaybes): self
+	public function intersectVariables(Scope $otherScope): self
 	{
 		$ourVariableTypeHolders = $this->getVariableTypes();
 		$theirVariableTypeHolders = $otherScope->getVariableTypes();
 		$intersectedVariableTypeHolders = [];
 		foreach ($theirVariableTypeHolders as $name => $variableTypeHolder) {
-			if (!$this->isSpecified(new Variable($name))) {
-				if (isset($ourVariableTypeHolders[$name])) {
-					$intersectedVariableTypeHolders[$name] = $ourVariableTypeHolders[$name]->and($variableTypeHolder);
-				} elseif ($addMaybes) {
-					$intersectedVariableTypeHolders[$name] = VariableTypeHolder::createMaybe($variableTypeHolder->getType());
-				}
+			if (isset($ourVariableTypeHolders[$name])) {
+				$intersectedVariableTypeHolders[$name] = $ourVariableTypeHolders[$name]->and($variableTypeHolder);
+			} else {
+				$intersectedVariableTypeHolders[$name] = VariableTypeHolder::createMaybe($variableTypeHolder->getType());
 			}
 		}
 
 		foreach ($ourVariableTypeHolders as $name => $variableTypeHolder) {
+			$variableNode = new Variable($name);
+			if ($otherScope->isSpecified($variableNode)) {
+				$intersectedVariableTypeHolders[$name] = VariableTypeHolder::createYes($otherScope->getType($variableNode));
+				continue;
+			}
 			if (isset($theirVariableTypeHolders[$name])) {
 				continue;
 			}
@@ -1411,8 +1414,12 @@ class Scope
 		$specifiedTypes = $this->moreSpecificTypes;
 		foreach ($intersectedScope->getVariableTypes() as $name => $theirVariableTypeHolder) {
 			if (isset($variableTypeHolders[$name])) {
+				$type = $theirVariableTypeHolder->getType();
+				if ($theirVariableTypeHolder->getCertainty()->maybe()) {
+					$type = $type->combineWith($variableTypeHolders[$name]->getType());
+				}
 				$theirVariableTypeHolder = new VariableTypeHolder(
-					$theirVariableTypeHolder->getType(),
+					$type,
 					$theirVariableTypeHolder->getCertainty()->or($variableTypeHolders[$name]->getCertainty())
 				);
 			}
@@ -1506,10 +1513,7 @@ class Scope
 			} else {
 				if (
 					isset($ourVariableTypeHolders[$name])
-					&& (
-						$theirVariableTypeHolder->getType()->describe() === $ourVariableTypeHolders[$name]->getType()->describe()
-						|| $this->isSpecified(new Variable($name))
-					)
+					&& $theirVariableTypeHolder->getType()->describe() === $ourVariableTypeHolders[$name]->getType()->describe()
 					&& $ourVariableTypeHolders[$name]->getCertainty()->equals($theirVariableTypeHolder->getCertainty())
 				) {
 					unset($ourVariableTypeHolders[$name]);

src/Analyser/NodeScopeResolver.php
@@ -1079,15 +1079,15 @@ class NodeScopeResolver
 		/** @var \PHPStan\Analyser\Scope|null $intersectedScope */
 		$intersectedScope = null;
 
-		/** @var \PHPStan\Analyser\Scope|null $allBranchesScope */
-		$allBranchesScope = null;
-
 		/** @var \PHPStan\Analyser\Scope|null $previousBranchScope */
 		$previousBranchScope = null;
 
 		foreach ($statementsLists as $i => $statementList) {
 			$statements = $statementList->getStatements();
 			$branchScope = $statementList->getScope();
+			if ($previousBranchScope !== null) {
+				$branchScope = $branchScope->addVariables($previousBranchScope);
+			}
 			$branchScopeWithInitialScopeRemoved = $branchScope->removeVariables($initialScope, true);
 
 			$earlyTerminationStatement = null;
@@ -1107,32 +1107,25 @@ class NodeScopeResolver
 				if ($intersectedScope === null) {
 					$intersectedScope = $initialScope->createIntersectedScope($branchScopeWithInitialScopeRemoved);
 				}
-				if ($allBranchesScope === null) {
-					$allBranchesScope = $initialScope->createIntersectedScope($branchScopeWithInitialScopeRemoved);
-				}
 
-				if ($isSwitchCase && $previousBranchScope !== null) {
-					$intersectedScope = $branchScopeWithInitialScopeRemoved->addVariables($previousBranchScope);
-					$allBranchesScope = $allBranchesScope->addVariables($previousBranchScope);
+				if ($isSwitchCase && $earlyTerminationStatement === null) {
+					$previousBranchScope = $branchScopeWithInitialScopeRemoved;
 				} else {
-					$intersectedScope = $intersectedScope->intersectVariables($branchScopeWithInitialScopeRemoved, true);
-					$allBranchesScope = $allBranchesScope->addVariables($branchScopeWithInitialScopeRemoved);
+					if ($previousBranchScope !== null) {
+						$intersectedScope = $intersectedScope->addVariables($previousBranchScope);
+					}
+					$intersectedScope = $intersectedScope->intersectVariables($branchScopeWithInitialScopeRemoved);
+					$previousBranchScope = null;
 				}
-			}
-
-			if ($earlyTerminationStatement === null) {
-				$previousBranchScope = $branchScopeWithInitialScopeRemoved;
-			} else {
-				$previousBranchScope = null;
 			}
 		}
 
-		if ($intersectedScope !== null && $allBranchesScope !== null) {
-			$allBranchesScope = $allBranchesScope->removeVariables($intersectedScope, true);
-			$allBranchesScope = $allBranchesScope->intersectVariables($initialScope, false);
-			$scope = $initialScope
-				->mergeWithIntersectedScope($intersectedScope)
-				->mergeWithIntersectedScope($allBranchesScope);
+		if ($previousBranchScope !== null && $intersectedScope !== null) {
+			$intersectedScope = $intersectedScope->intersectVariables($previousBranchScope);
+		}
+
+		if ($intersectedScope !== null) {
+			$scope = $initialScope->mergeWithIntersectedScope($intersectedScope);
 
 			return $scope;
 		}

commit 50acb7112a36c256ad15061e2cb9c83dbefc6d7c

Fixed regression bug after 22488d5aa657830ccbb72bcd666b7b6f55b5257a

src/Analyser/Scope.php
@@ -1332,7 +1332,12 @@ class Scope
 		foreach ($ourVariableTypeHolders as $name => $variableTypeHolder) {
 			$variableNode = new Variable($name);
 			if ($otherScope->isSpecified($variableNode)) {
-				$intersectedVariableTypeHolders[$name] = VariableTypeHolder::createYes($otherScope->getType($variableNode));
+				$intersectedVariableTypeHolders[$name] = VariableTypeHolder::createYes(
+					TypeCombinator::union(
+						$otherScope->getType($variableNode),
+						$variableTypeHolder->getType()
+					)
+				);
 				continue;
 			}
 			if (isset($theirVariableTypeHolders[$name])) {

tests/PHPStan/Analyser/NodeScopeResolverTest.php
@@ -481,6 +481,18 @@ class NodeScopeResolverTest extends \PHPStan\TestCase
 			],
 			[
 				$testScope,
+				'integerOrString',
+				TrinaryLogic::createYes(),
+				'int|string',
+			],
+			[
+				$testScope,
+				'nullableIntegerAfterNeverCondition',
+				TrinaryLogic::createYes(),
+				'int|null',
+			],
+			[
+				$testScope,
 				'arrayOfIntegers',
 				TrinaryLogic::createYes(),
 				'int[]',

tests/PHPStan/Analyser/data/if.php
@@ -238,6 +238,18 @@ if ($nullableString !== null) {
 	$whatever = $nullableString;
 }
 
+/** @var int|null $integerOrString */
+$integerOrString = 1;
+if ($integerOrString === null) {
+	$integerOrString = 'str';
+}
+
+/** @var int|null $nullableIntegerAfterNeverCondition */
+$nullableIntegerAfterNeverCondition = 1;
+if ($nullableIntegerAfterNeverCondition === false) {
+	$nullableIntegerAfterNeverCondition = 1;
+}
+
 $arrayOfIntegers = [1, 2, 3];
 
 $arrayAccessObject = new \ObjectWithArrayAccess\Foo();

tests/PHPStan/Rules/Methods/ReturnTypeRuleTest.php
@@ -169,12 +169,16 @@ class ReturnTypeRuleTest extends \PHPStan\Rules\AbstractRuleTest
 				320,
 			],
 			[
+				'Method ReturnTypes\Stock::returnYetSelfAgainError() should return ReturnTypes\Stock but returns ReturnTypes\Stock|null.',
+				344,
+			],
+			[
 				'Method ReturnTypes\ReturningSomethingFromConstructor::__construct() with return type void returns ReturnTypes\Foo but should not return anything.',
-				377,
+				388,
 			],
 			[
 				'Method ReturnTypes\WeirdReturnFormat::test() should return PHPStan\Foo\Bar but returns int.',
-				391,
+				402,
 			],
 		]);
 	}

tests/PHPStan/Rules/Methods/data/returnTypes.php
@@ -320,7 +320,8 @@ class Stock
 		return $stock; // still possible null
 	}
 
-	public function returnsSelfAgainAgain(): self {
+	public function returnsSelfAgainAgain(): self
+	{
 		while (true) {
 			try {
 				if ($this->getActualStock() === null) {
@@ -331,6 +332,16 @@ class Stock
 			}
 			return $this->getActualStock();
 		}
+	}
+
+	public function returnYetSelfAgainError(): self
+	{
+		$stock = $this->findStock();
+		if ($stock === false) {
+			$stock = new self();
+		}
+
+		return $stock; // still possible null
 	}
 
 }