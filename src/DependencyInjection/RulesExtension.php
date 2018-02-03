4dfbf4bbf10bf1b8c5cdb941dcebdbd6cdf9dfb6
Ondrej Mirtes <ondrej@mirtes.cz>
2018-02-03 11:13:51 +0100

    Tighter config of enabled rules thanks to RulesExtension

---
+++ src/DependencyInjection/RulesExtension.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\DependencyInjection;
+
+use PHPStan\Rules\RegistryFactory;
+
+class RulesExtension extends \Nette\DI\CompilerExtension
+{
+
+	public function loadConfiguration()
+	{
+		$config = $this->config;
+		$builder = $this->getContainerBuilder();
+
+		foreach ($config as $key => $rule) {
+			$builder->addDefinition($this->prefix((string) $key))
+				->setFactory($rule)
+				->setAutowired(false)
+				->addTag(RegistryFactory::RULE_TAG);
+		}
+	}
+
+}
