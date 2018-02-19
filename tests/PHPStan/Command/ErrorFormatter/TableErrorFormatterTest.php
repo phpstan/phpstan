66b6c6c4346c50fb6bf6b381f46f153142e27b64
Marco Pivetta <ocramius@gmail.com>
2018-02-19 20:11:21 +0100

    #838 expect static analysis results to be sorted by file name

---
+++ tests/PHPStan/Command/ErrorFormatter/TableErrorFormatterTest.php
@@ -0,0 +1,98 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Command\ErrorFormatter;
+
+use PHPStan\Analyser\Error;
+use PHPStan\Command\AnalysisResult;
+use PHPStan\Command\ErrorsConsoleStyle;
+use Symfony\Component\Console\Input\StringInput;
+use Symfony\Component\Console\Output\StreamOutput;
+
+class TableErrorFormatterTest extends \PHPStan\Testing\TestCase
+{
+	/**
+	 * @var TableErrorFormatter
+	 */
+	protected $formatter;
+
+    /**
+     * {@inheritDoc}
+     */
+	protected function setUp(): void
+	{
+		$this->formatter = new TableErrorFormatter();
+	}
+
+	/**
+     * @group #838
+	 */
+	public function testErrorsAreDisplayedInAlphabeticalOrder(): void
+	{
+		$analysisResultMock = $this->createMock(AnalysisResult::class);
+
+		$analysisResultMock
+			->expects(self::any())
+			->method('hasErrors')
+			->willReturn(true);
+
+		$analysisResultMock
+			->expects(self::any())
+			->method('getFileSpecificErrors')
+			->willReturn([
+				new Error('Message 1', 'b', 1),
+				new Error('Message 2', 'a', 1),
+				new Error('Message 3', 'd', 1),
+				new Error('Message 4', 'c', 1),
+				new Error('Message 5', '1', 1),
+			]);
+
+		$resource = fopen('php://memory', 'wb', false);
+
+		self::assertInternalType('resource', $resource);
+
+		$outputStream = new StreamOutput($resource);
+		$style = new ErrorsConsoleStyle(new StringInput(''), $outputStream);
+
+		self::assertSame(1, $this->formatter->formatErrors($analysisResultMock, $style));
+
+		rewind($outputStream->getStream());
+
+		self::assertStringMatchesFormat(
+		    <<<'REPORT'
+%A------ ----------- 
+  Line   a          
+ ------ ----------- 
+  1      Message 2  
+ ------ ----------- 
+
+ ------ ----------- 
+  Line   b          
+ ------ ----------- 
+  1      Message 1  
+ ------ ----------- 
+
+ ------ ----------- 
+  Line   c          
+ ------ ----------- 
+  1      Message 4  
+ ------ ----------- 
+
+ ------ ----------- 
+  Line   d          
+ ------ ----------- 
+  1      Message 3  
+ ------ ----------- 
+
+ ------ ----------- 
+  Line   1          
+ ------ ----------- 
+  1      Message 5  
+ ------ ----------- 
+
+ [ERROR] Found 0 errors%A
+REPORT
+,
+            stream_get_contents($outputStream->getStream())
+        );
+	}
+}
