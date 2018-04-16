4e1b2d831a810d4ab20bdab029da9b12c34c3b7e
Ondrej Mirtes <ondrej@mirtes.cz>
2018-04-16 22:16:15 +0200

    is_subclass_of() type-specifying extension

---
+++ src/Type/Php/IsSubclassOfFunctionTypeSpecifyingExtension.php
@@ -0,0 +1,55 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Type\Php;
+
+use PhpParser\Node\Expr\FuncCall;
+use PHPStan\Analyser\Scope;
+use PHPStan\Analyser\SpecifiedTypes;
+use PHPStan\Analyser\TypeSpecifier;
+use PHPStan\Analyser\TypeSpecifierAwareExtension;
+use PHPStan\Analyser\TypeSpecifierContext;
+use PHPStan\Reflection\FunctionReflection;
+use PHPStan\Type\Constant\ConstantStringType;
+use PHPStan\Type\FunctionTypeSpecifyingExtension;
+
+class IsSubclassOfFunctionTypeSpecifyingExtension implements FunctionTypeSpecifyingExtension, TypeSpecifierAwareExtension
+{
+
+	/** @var \PHPStan\Analyser\TypeSpecifier */
+	private $typeSpecifier;
+
+	public function isFunctionSupported(FunctionReflection $functionReflection, FuncCall $node, TypeSpecifierContext $context): bool
+	{
+		return strtolower($functionReflection->getName()) === 'is_subclass_of'
+			&& count($node->args) >= 2
+			&& !$context->null();
+	}
+
+	public function specifyTypes(FunctionReflection $functionReflection, FuncCall $node, Scope $scope, TypeSpecifierContext $context): SpecifiedTypes
+	{
+		$objectType = $scope->getType($node->args[0]->value);
+		if ((new \PHPStan\Type\ObjectWithoutClassType())->isSuperTypeOf($objectType)->no()) {
+			return new SpecifiedTypes();
+		}
+
+		$classType = $scope->getType($node->args[1]->value);
+		if (!$classType instanceof ConstantStringType) {
+			return new SpecifiedTypes();
+		}
+
+		return $this->typeSpecifier->specifyTypesInCondition(
+			$scope,
+			new \PhpParser\Node\Expr\Instanceof_(
+				$node->args[0]->value,
+				new \PhpParser\Node\Name($classType->getValue())
+			),
+			$context
+		);
+	}
+
+	public function setTypeSpecifier(TypeSpecifier $typeSpecifier): void
+	{
+		$this->typeSpecifier = $typeSpecifier;
+	}
+
+}
