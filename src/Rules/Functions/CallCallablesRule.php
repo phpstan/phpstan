dad2560a64f7433d0a76c8f1ca6bcc76f2d83344
Ondrej Mirtes <ondrej@mirtes.cz>
2018-04-24 14:42:52 +0200

    Check whether $c() is a callable

---
+++ src/Rules/Functions/CallCallablesRule.php
@@ -0,0 +1,58 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Rules\Functions;
+
+use PHPStan\Analyser\Scope;
+use PHPStan\Type\MixedType;
+use PHPStan\Type\VerbosityLevel;
+
+class CallCallablesRule implements \PHPStan\Rules\Rule
+{
+
+	/** @var bool */
+	private $reportMaybes;
+
+	public function __construct(bool $reportMaybes)
+	{
+		$this->reportMaybes = $reportMaybes;
+	}
+
+	public function getNodeType(): string
+	{
+		return \PhpParser\Node\Expr\FuncCall::class;
+	}
+
+	/**
+	 * @param \PhpParser\Node\Expr\FuncCall $node
+	 * @param \PHPStan\Analyser\Scope $scope
+	 * @return string[]
+	 */
+	public function processNode(
+		\PhpParser\Node $node,
+		Scope $scope
+	): array
+	{
+		if (!$node->name instanceof \PhpParser\Node\Expr) {
+			return [];
+		}
+
+		$exprType = $scope->getType($node->name);
+		$isCallable = $exprType->isCallable();
+		if ($isCallable->no()) {
+			return [
+				sprintf('Trying to invoke %s but it\'s not a callable.', $exprType->describe(VerbosityLevel::value())),
+			];
+		} elseif (
+			$this->reportMaybes
+			&& !$isCallable->yes()
+			&& !$exprType instanceof MixedType
+		) {
+			return [
+				sprintf('Trying to invoke %s but it might not a callable.', $exprType->describe(VerbosityLevel::value())),
+			];
+		}
+
+		return [];
+	}
+
+}
---
+++ tests/PHPStan/Rules/Functions/CallCallablesRuleTest.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Rules\Functions;
+
+class CallCallablesRuleTest extends \PHPStan\Testing\RuleTestCase
+{
+
+	protected function getRule(): \PHPStan\Rules\Rule
+	{
+		return new CallCallablesRule(true);
+	}
+
+	public function testRule(): void
+	{
+		$this->analyse([__DIR__ . '/data/callables.php'], [
+			[
+				'Trying to invoke string but it might not a callable.',
+				17,
+			],
+			[
+				'Trying to invoke \'nonexistent\' but it\'s not a callable.',
+				24,
+			],
+		]);
+	}
+
+}
