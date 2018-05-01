34d47a8db04ab865abd12eb8f21172e58e881487
Gabriel Caruso <carusogabriel34@gmail.com>
2018-05-01 20:05:16 -0300

    Use Node\Expr when searching for reading/writing only properties

--- src/Rules/Properties/ReadingWriteOnlyPropertiesRule.php
+++ src/Rules/Properties/ReadingWriteOnlyPropertiesRule.php
@@ -36,11 +36,11 @@ class ReadingWriteOnlyPropertiesRule implements \PHPStan\Rules\Rule
 
 	public function getNodeType(): string
 	{
-		return \PhpParser\Node::class;
+		return \PhpParser\Node\Expr::class;
 	}
 
 	/**
-	 * @param \PhpParser\Node $node
+	 * @param \PhpParser\Node\Expr $node
 	 * @param Scope $scope
 	 * @return string[]
 	 */
