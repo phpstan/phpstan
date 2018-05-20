45ae49f688661316b540a163f11d1b13c40f8a4f
Ondrej Mirtes <ondrej@mirtes.cz>
2018-05-20 20:09:21 +0200

    Test - default property array does not fall back to value type mixed

--- tests/PHPStan/Rules/Properties/data/properties-assigned-default-value-types.php
+++ tests/PHPStan/Rules/Properties/data/properties-assigned-default-value-types.php
@@ -20,4 +20,10 @@ class Foo
 	/** @var string */
 	private $stringPropertyWithDefaultNullValue = null;
 
+	/** @var array<string,string> */
+	private static $windowsNtVersions = [
+		'2000' => '5.0',
+		'xp' => '5.1',
+	];
+
 }
