87ea316e3d79ec49928800c3b6a4cb97b7c10285
Nat Zimmermann <ntzm@users.noreply.github.com>
2018-06-24 22:37:44 +0100

    SplFixedArray::__construct argument is optional

--- src/Reflection/SignatureMap/functionMap.php
+++ src/Reflection/SignatureMap/functionMap.php
@@ -11013,7 +11013,7 @@ return [
 'SplFileObject::setFlags' => ['void', 'flags'=>'int'],
 'SplFileObject::setMaxLineLen' => ['void', 'max_len'=>'int'],
 'SplFileObject::valid' => ['bool'],
-'SplFixedArray::__construct' => ['void', 'size'=>'int'],
+'SplFixedArray::__construct' => ['void', 'size='=>'int'],
 'SplFixedArray::__wakeup' => ['void'],
 'SplFixedArray::count' => ['int'],
 'SplFixedArray::current' => ['mixed'],
