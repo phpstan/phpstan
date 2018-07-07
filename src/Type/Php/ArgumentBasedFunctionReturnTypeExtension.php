282d64aee713b933d6951f76d1f3a899a507a275
Ondřej Vodáček <ondrej.vodacek@gmail.com>
2018-07-07 15:20:37 +0200

    add more array_ functions that return same type as first argument

--- src/Type/Php/ArgumentBasedFunctionReturnTypeExtension.php
+++ src/Type/Php/ArgumentBasedFunctionReturnTypeExtension.php
@@ -16,6 +16,23 @@ class ArgumentBasedFunctionReturnTypeExtension implements \PHPStan\Type\DynamicF
 	private $functionNames = [
 		'array_unique' => 0,
 		'array_reverse' => 0,
+		'array_change_key_case' => 0,
+		'array_diff_assoc' => 0,
+		'array_diff_key' => 0,
+		'array_diff_uassoc' => 0,
+		'array_diff_ukey' => 0,
+		'array_diff' => 0,
+		'array_udiff_assoc' => 0,
+		'array_udiff_uassoc' => 0,
+		'array_udiff' => 0,
+		'array_intersect_assoc' => 0,
+		'array_intersect_key' => 0,
+		'array_intersect_uassoc' => 0,
+		'array_intersect_ukey' => 0,
+		'array_intersect' => 0,
+		'array_uintersect_assoc' => 0,
+		'array_uintersect_uassoc' => 0,
+		'array_uintersect' => 0,
 	];
 
 	public function isFunctionSupported(FunctionReflection $functionReflection): bool
