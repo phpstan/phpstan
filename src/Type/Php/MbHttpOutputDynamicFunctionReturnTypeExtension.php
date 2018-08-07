6a3d9857fa74c74eb23ad511985fa44da1bf98da
Trevor Rowbotham <smokey101stair@gmail.com>
2018-08-07 00:41:52 -0400

    Add support for mb_http_output

---
+++ src/Type/Php/MbHttpOutputDynamicFunctionReturnTypeExtension.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Type\Php;
+
+use PhpParser\Node\Expr\FuncCall;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\FunctionReflection;
+use PHPStan\Type\BooleanType;
+use PHPStan\Type\Constant\ConstantBooleanType;
+use PHPStan\Type\Constant\ConstantStringType;
+use PHPStan\Type\StringType;
+use PHPStan\Type\Type;
+use PHPStan\Type\TypeUtils;
+
+class MbHttpOutputDynamicFunctionReturnTypeExtension extends MbStringAbstractBaseDynamicFunctionReturnTypeExtension
+{
+
+	public function isFunctionSupported(FunctionReflection $functionReflection): bool
+	{
+		return $functionReflection->getName() === 'mb_http_output' && $this->isMbStringExtensionLoaded();
+	}
+
+	public function getTypeFromFunctionCall(FunctionReflection $functionReflection, FuncCall $functionCall, Scope $scope): Type
+	{
+		if (!isset($functionCall->args[0])) {
+			return new StringType();
+		}
+
+		$results = array_unique(array_map(function (ConstantStringType $encoding): bool {
+			return $this->isSupportedEncoding($encoding->getValue());
+		}, TypeUtils::getConstantStrings($scope->getType($functionCall->args[0]->value))));
+
+		if (count($results) !== 1) {
+			return new BooleanType();
+		}
+
+		return new ConstantBooleanType($results[0]);
+	}
+
+}
