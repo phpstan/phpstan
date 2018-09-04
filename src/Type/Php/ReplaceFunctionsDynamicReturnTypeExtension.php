99075831f96f014ad3a81f36a6dcc6adb3799976
Dmitry Dymarchuk <ddymarchuk@alternion.com>
2018-09-04 23:43:10 +0300

    Add null as returning type for preg_replace like functions

--- src/Type/Php/ReplaceFunctionsDynamicReturnTypeExtension.php
+++ src/Type/Php/ReplaceFunctionsDynamicReturnTypeExtension.php
@@ -9,21 +9,23 @@ use PHPStan\Reflection\ParametersAcceptorSelector;
 use PHPStan\Type\ArrayType;
 use PHPStan\Type\DynamicFunctionReturnTypeExtension;
 use PHPStan\Type\MixedType;
+use PHPStan\Type\NullType;
 use PHPStan\Type\StringType;
 use PHPStan\Type\Type;
 use PHPStan\Type\TypeUtils;
+use PHPStan\Type\UnionType;
 
 class ReplaceFunctionsDynamicReturnTypeExtension implements DynamicFunctionReturnTypeExtension
 {
 
 	/** @var array<string, int> */
 	private $functions = [
-		'preg_replace' => 2,
-		'preg_replace_callback' => 2,
-		'preg_replace_callback_array' => 1,
-		'str_replace' => 2,
-		'str_ireplace' => 2,
-		'substr_replace' => 0,
+		'preg_replace' => ['argumentPosition' => 2, 'returnNull' => true],
+		'preg_replace_callback' => ['argumentPosition' => 2, 'returnNull' => true],
+		'preg_replace_callback_array' => ['argumentPosition' => 1, 'returnNull' => true],
+		'str_replace' => ['argumentPosition' => 2, 'returnNull' => false],
+		'str_ireplace' => ['argumentPosition' => 2, 'returnNull' => false],
+		'substr_replace' => ['argumentPosition' => 0, 'returnNull' => false],
 	];
 
 	public function isFunctionSupported(FunctionReflection $functionReflection): bool
@@ -37,7 +39,29 @@ class ReplaceFunctionsDynamicReturnTypeExtension implements DynamicFunctionRetur
 		Scope $scope
 	): Type
 	{
-		$argumentPosition = $this->functions[$functionReflection->getName()];
+		$type = $this->getPreliminarilyResolvedTypeFromFunctionCall($functionReflection, $functionCall, $scope);
+
+		if ($this->functions[$functionReflection->getName()]['returnNull']) {
+			$type = $type instanceof UnionType
+				? new UnionType(
+					array_merge(
+						array_filter($type->getTypes(), function (Type $type) { return !$type instanceof NullType; }),
+						[new NullType()]
+					)
+				)
+				: new UnionType([$type, new NullType()]);
+		}
+
+		return $type;
+	}
+
+	public function getPreliminarilyResolvedTypeFromFunctionCall(
+		FunctionReflection $functionReflection,
+		FuncCall $functionCall,
+		Scope $scope
+	): Type
+	{
+		$argumentPosition = $this->functions[$functionReflection->getName()]['argumentPosition'];
 		if (count($functionCall->args) <= $argumentPosition) {
 			return ParametersAcceptorSelector::selectSingle($functionReflection->getVariants())->getReturnType();
 		}
