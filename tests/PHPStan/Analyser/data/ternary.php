21358372a1fac39449a54f84cf204dedefbcddb5
Lukáš Unger <looky.msc@gmail.com>
2018-12-03 12:52:21 +0100

    Fix condition filtering in short ternary operator

--- tests/PHPStan/Analyser/data/ternary.php
+++ tests/PHPStan/Analyser/data/ternary.php
@@ -4,4 +4,16 @@
 $boolOrNull = doFoo();
 $bool = $boolOrNull !== null ? $boolOrNull : false;
 
+$short = $bool ?: null;
+
+/** @var bool $a */
+$a = doBar();
+/** @var bool $b */
+$b = doBaz();
+$c = $a ?: $b;
+
+/** @var string|null $qux */
+$qux = doQux();
+$isQux = $qux !== null ?: $bool;
+
 die;
