ed4ebbbd7d2b8f0aeb4a4f9d953bffbaf563e296
Lukáš Unger <looky.msc@gmail.com>
2019-01-31 15:25:56 +0100

    Added possibility to override rule error file

--- src/Rules/RuleErrorBuilder.php
+++ src/Rules/RuleErrorBuilder.php
@@ -3,7 +3,9 @@
 namespace PHPStan\Rules;
 
 use PHPStan\Rules\RuleErrors\RuleErrorWithMessage;
+use PHPStan\Rules\RuleErrors\RuleErrorWithMessageAndFile;
 use PHPStan\Rules\RuleErrors\RuleErrorWithMessageAndLine;
+use PHPStan\Rules\RuleErrors\RuleErrorWithMessageAndLineAndFile;
 
 class RuleErrorBuilder
 {
@@ -13,6 +15,9 @@ class RuleErrorBuilder
 
 	/** @var int|null */
 	private $line;
+
+	/** @var string|null */
+	private $file;
 
 	private function __construct()
 	{
@@ -33,10 +38,23 @@ class RuleErrorBuilder
 		return $this;
 	}
 
+	public function file(string $file): self
+	{
+		$this->file = $file;
+
+		return $this;
+	}
+
 	public function build(): RuleError
 	{
-		if ($this->line !== null) {
+		if ($this->line !== null && $this->file !== null) {
+			return new RuleErrorWithMessageAndLineAndFile($this->message, $this->line, $this->file);
+		}
+		if ($this->line !== null && $this->file === null) {
 			return new RuleErrorWithMessageAndLine($this->message, $this->line);
+		}
+		if ($this->line === null && $this->file !== null) {
+			return new RuleErrorWithMessageAndFile($this->message, $this->file);
 		}
 
 		return new RuleErrorWithMessage($this->message);
