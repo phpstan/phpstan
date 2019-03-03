21352c6223f2f878ed794be69342676ffda5bd1b
Ondrej Mirtes <ondrej@mirtes.cz>
2019-03-03 16:59:22 +0100

    Test - closes #1684

--- tests/PHPStan/Rules/Comparison/data/ternary.php
+++ tests/PHPStan/Rules/Comparison/data/ternary.php
@@ -15,4 +15,27 @@ class Ternary
 		$zero ? 'foo' : 'bar';
 	}
 
+	public function doBar(array $a)
+	{
+		$a ? 1 : 2;
+		$a ? 3 : 4;
+	}
+
+	public function doBaz(array $a)
+	{
+		if (!$a) {
+		}
+
+		print $a ? 'aa' : 'bb';
+	}
+
+	public function doLorem(array $a)
+	{
+		if (!$a || $a['a']) {
+		} elseif ($a['b']) {
+		}
+
+		print $a ? 'aa' : 'bb';
+	}
+
 }
