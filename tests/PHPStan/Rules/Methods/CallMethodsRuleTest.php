ff7169ad5fe0e8b942f724299e80c4bb2cef261c
Ondrej Mirtes <ondrej@mirtes.cz>
2019-03-03 17:32:23 +0100

    Test - closes #1595

--- tests/PHPStan/Rules/Methods/CallMethodsRuleTest.php
+++ tests/PHPStan/Rules/Methods/CallMethodsRuleTest.php
@@ -397,6 +397,10 @@ class CallMethodsRuleTest extends \PHPStan\Testing\RuleTestCase
 				'Call to an undefined method Test\ArraySliceWithNonEmptyArray::doesNotExist().',
 				1092,
 			],
+			[
+				'Call to an undefined method Test\AssertInFor::doBar().',
+				1207,
+			],
 		]);
 	}
 
