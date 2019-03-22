c952192e7185376c0f722937982d055cab0bebc8
Ondrej Mirtes <ondrej@mirtes.cz>
2019-03-22 12:05:53 +0100

    Yield checks (level 3, enable with bleedingEdge.neon now, will be enabled by default in 0.12)

---
+++ src/Rules/Generators/YieldInGeneratorRule.php
@@ -0,0 +1,74 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Rules\Generators;
+
+use PhpParser\Node;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\ParametersAcceptorSelector;
+use PHPStan\Rules\Rule;
+use PHPStan\Type\IterableType;
+use PHPStan\Type\MixedType;
+
+class YieldInGeneratorRule implements Rule
+{
+
+	/** @var bool */
+	private $reportMaybes;
+
+	public function __construct(bool $reportMaybes)
+	{
+		$this->reportMaybes = $reportMaybes;
+	}
+
+	public function getNodeType(): string
+	{
+		return Node\Expr::class;
+	}
+
+	/**
+	 * @param Node\Expr $node
+	 * @param Scope $scope
+	 * @return string[]
+	 */
+	public function processNode(Node $node, Scope $scope): array
+	{
+		if (!$node instanceof Node\Expr\Yield_ && !$node instanceof Node\Expr\YieldFrom) {
+			return [];
+		}
+
+		$anonymousFunctionReturnType = $scope->getAnonymousFunctionReturnType();
+		$scopeFunction = $scope->getFunction();
+		if ($anonymousFunctionReturnType !== null) {
+			$returnType = $anonymousFunctionReturnType;
+		} elseif ($scopeFunction !== null) {
+			$returnType = ParametersAcceptorSelector::selectSingle($scopeFunction->getVariants())->getReturnType();
+		} else {
+			return [
+				'Yield can be used only inside a function.',
+			];
+		}
+
+		if ($returnType instanceof MixedType) {
+			return [];
+		}
+
+		$iterableType = new IterableType(new MixedType(), new MixedType());
+
+		$isSuperType = $iterableType->isSuperTypeOf($returnType);
+		if ($isSuperType->yes()) {
+			return [];
+		}
+
+		if ($isSuperType->maybe() && !$this->reportMaybes) {
+			return [];
+		}
+
+		return [
+			sprintf(
+				'Yield can be used only with these return types: %s.',
+				'Generator, Iterator, Traversable, iterable'
+			),
+		];
+	}
+
+}
---
+++ tests/PHPStan/Rules/Generators/data/yield-outside-function.php
@@ -0,0 +1,6 @@
+<?php // lint <= 7.0
+
+namespace YieldOutsideFunction;
+
+yield 1;
+yield from doFoo();
