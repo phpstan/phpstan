cdf01c9ca379e618d042ac15ea6787f8f9bc3454
Lars Moelleken <lars.moelleken@vdmg-it.com>
2019-04-11 08:43:19 +0200

    update the "functionMap" v2.3
    -> fix tests

--- tests/PHPStan/Rules/Operators/InvalidBinaryOperationRuleTest.php
+++ tests/PHPStan/Rules/Operators/InvalidBinaryOperationRuleTest.php
@@ -95,11 +95,11 @@ class InvalidBinaryOperationRuleTest extends \PHPStan\Testing\RuleTestCase
 				127,
 			],
 			[
-				'Binary operation "." between array|string and \'xyz\' results in an error.',
+				'Binary operation "." between array<string>|string and \'xyz\' results in an error.',
 				134,
 			],
 			[
-				'Binary operation "+" between (array|string) and 1 results in an error.',
+				'Binary operation "+" between (array<string>|string) and 1 results in an error.',
 				136,
 			],
 			[
