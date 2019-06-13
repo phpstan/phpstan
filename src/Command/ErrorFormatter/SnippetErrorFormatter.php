de187e65879de58d933cba643cc5fecb6607959b
Alexander Loutsenko <alex.chorry@gmail.com>
2019-06-14 02:40:01 +0300

    snippetFormatter now is a json

--- src/Command/ErrorFormatter/SnippetErrorFormatter.php
+++ src/Command/ErrorFormatter/SnippetErrorFormatter.php
@@ -2,38 +2,62 @@
 
 namespace PHPStan\Command\ErrorFormatter;
 
+use Nette\Utils\Json;
 use PHPStan\Command\AnalysisResult;
 use Symfony\Component\Console\Style\OutputStyle;
 
 class SnippetErrorFormatter implements ErrorFormatter
 {
 
+	/** @var bool */
+	private $pretty;
+
+	public function __construct(bool $pretty)
+	{
+		$this->pretty = $pretty;
+	}
+
 	public function formatErrors(
 		AnalysisResult $analysisResult,
 		OutputStyle $style
 	): int
 	{
-		if (!$analysisResult->hasErrors()) {
-			return 0;
+		$errorsArray = [
+			'totals' => [
+				'errors' => count($analysisResult->getNotFileSpecificErrors()),
+				'file_errors' => count($analysisResult->getFileSpecificErrors()),
+			],
+			'files' => [],
+			'errors' => [],
+		];
+
+		foreach ($analysisResult->getFileSpecificErrors() as $fileSpecificError) {
+			$file = $fileSpecificError->getFile();
+			if (!array_key_exists($file, $errorsArray['files'])) {
+				$errorsArray['files'][$file] = [
+					'errors' => 0,
+					'messages' => [],
+				];
+			}
+			$errorsArray['files'][$file]['errors']++;
+
+			$errorsArray['files'][$file]['messages'][] = [
+				'message' => $fileSpecificError->getMessage(),
+				'line' => $fileSpecificError->getLine(),
+				'ignorable' => $fileSpecificError->canBeIgnored(),
+				'snippet' => $fileSpecificError->getSnippet(),
+			];
 		}
 
 		foreach ($analysisResult->getNotFileSpecificErrors() as $notFileSpecificError) {
-			$style->writeln(sprintf('?:?:%s', $notFileSpecificError));
+			$errorsArray['errors'][] = $notFileSpecificError;
 		}
 
-		foreach ($analysisResult->getFileSpecificErrors() as $fileSpecificError) {
-			$style->writeln(
-				sprintf(
-					'%s:%d:%s',
-					$fileSpecificError->getFile(),
-					$fileSpecificError->getLine() ?? '?',
-					$fileSpecificError->getMessage()
-				)
-			);
-			$style->writeln($fileSpecificError->getSnippet());
-		}
+		$json = Json::encode($errorsArray, $this->pretty ? Json::PRETTY : 0);
 
-		return 1;
+		$style->write($json);
+
+		return $analysisResult->hasErrors() ? 1 : 0;
 	}
 
 }
