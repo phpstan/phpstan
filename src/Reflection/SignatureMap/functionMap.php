7d67b4fd24a0c21a70f28a406414adc128d25a0b
Georgi Georgiev <vuryss@gmail.com>
2019-09-02 13:18:57 +0300

    Updating Redis::exists return type

--- src/Reflection/SignatureMap/functionMap.php
+++ src/Reflection/SignatureMap/functionMap.php
@@ -9262,7 +9262,7 @@ return [
 'Redis::evaluate' => ['mixed', 'script'=>'string', 'args='=>'array', 'numKeys='=>'int'],
 'Redis::evaluateSha' => ['', 'scriptSha'=>'string', 'args='=>'array', 'numKeys='=>'int'],
 'Redis::exec' => ['array'],
-'Redis::exists' => ['bool', 'key'=>'string'],
+'Redis::exists' => ['int', 'key'=>'string'],
 'Redis::expire' => ['bool', 'key'=>'string', 'ttl'=>'int'],
 'Redis::expireAt' => ['bool', 'key'=>'string', 'expiry'=>'int'],
 'Redis::flushAll' => ['bool'],
