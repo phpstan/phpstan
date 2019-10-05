4f0890f3a125197190db4f2a2db20d781f9fcd5f
Ondrej Mirtes <ondrej@mirtes.cz>
2019-10-05 16:37:58 +0200

    Fix typo

--- src/Rules/Generics/TraitTemplateTypeRule.php
+++ src/Rules/Generics/TraitTemplateTypeRule.php
@@ -46,18 +46,18 @@ class TraitTemplateTypeRule implements Rule
 			throw new \PHPStan\ShouldNotHappenException();
 		}
 
-		$className = (string) $node->namespacedName;
+		$traitName = (string) $node->namespacedName;
 		$resolvedPhpDoc = $this->fileTypeMapper->getResolvedPhpDoc(
 			$scope->getFile(),
-			$className,
+			$traitName,
 			null,
 			$docComment->getText()
 		);
 
 		return $this->templateTypeCheck->check(
 			$resolvedPhpDoc->getTemplateTags(),
-			sprintf('PHPDoc tag @template for trait %s cannot have existing class %%s as its name.', $className),
-			sprintf('PHPDoc tag @template %%s for trait %s has invalid bound type %%s.', $className)
+			sprintf('PHPDoc tag @template for trait %s cannot have existing class %%s as its name.', $traitName),
+			sprintf('PHPDoc tag @template %%s for trait %s has invalid bound type %%s.', $traitName)
 		);
 	}
 
