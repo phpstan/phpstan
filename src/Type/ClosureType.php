a4adf2e6c621f5514242554b0625115cb7412c43
Jiří Pudil <me@jiripudil.cz>
2019-10-25 20:32:41 +0200

    Return a special method reflection for Closure::call()

--- src/Type/ClosureType.php
+++ src/Type/ClosureType.php
@@ -8,6 +8,7 @@ use PHPStan\Reflection\MethodReflection;
 use PHPStan\Reflection\Native\NativeParameterReflection;
 use PHPStan\Reflection\ParameterReflection;
 use PHPStan\Reflection\ParametersAcceptor;
+use PHPStan\Reflection\Php\ClosureCallMethodReflection;
 use PHPStan\Reflection\PropertyReflection;
 use PHPStan\TrinaryLogic;
 use PHPStan\Type\Constant\ConstantArrayType;
@@ -158,6 +159,13 @@ class ClosureType implements TypeWithClassName, ParametersAcceptor
 
 	public function getMethod(string $methodName, ClassMemberAccessAnswerer $scope): MethodReflection
 	{
+		if ($methodName === 'call') {
+			return new ClosureCallMethodReflection(
+				$this->objectType->getMethod($methodName, $scope),
+				$this
+			);
+		}
+
 		return $this->objectType->getMethod($methodName, $scope);
 	}
 
