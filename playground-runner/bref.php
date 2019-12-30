cfb2f2fa713b4e55523ea2a82a93dd78a06a128c
Ondrej Mirtes <ondrej@mirtes.cz>
2019-12-30 23:11:49 +0100

    Playground runner - 100% static reflection

--- playground-runner/bref.php
+++ playground-runner/bref.php
@@ -17,52 +17,10 @@ lambda(function (array $event) {
 	$codePath = '/tmp/tmp.php';
 	file_put_contents($codePath, $code);
 
-	$errorHandler = new \PhpParser\ErrorHandler\Collecting();
-	$sanitizer = new \App\CodeSanitizer(
-		new PhpParser\Parser\Php7(new \PhpParser\Lexer()),
-		new \PhpParser\PrettyPrinter\Standard(),
-		$errorHandler
-	);
-	$autoloadPath = '/tmp/autoload.php';
-	$sanitizedCode = $sanitizer->sanitize($code);
-	if ($errorHandler->hasErrors()) {
-		$errors = [];
-		foreach ($errorHandler->getErrors() as $error) {
-			$errors[] = [
-				'message' => $error->getRawMessage(),
-				'line' => $error->getStartLine(),
-			];
-		}
-		return [
-			'result' => $errors,
-			'version' => \Jean85\PrettyVersions::getVersion('phpstan/phpstan')->getPrettyVersion(),
-		];
-	}
-
 	$rootDir = getenv('LAMBDA_TASK_ROOT');
-	file_put_contents($autoloadPath, $sanitizedCode);
-	exec('php ' . escapeshellarg($autoloadPath), $outputArray, $exitCode);
-	if ($exitCode !== 0) {
-		$message = trim(implode("\n", $outputArray));
-		$line = 1;
-		if (preg_match('#on line (\d+)#', $message, $matches)) {
-			$line = (int) $matches[1];
-			$message = trim(str_replace('on line ' . $line, '', $message));
-		}
-		$message = trim(str_replace('in /tmp/autoload.php', '', $message));
-
-		return [
-			'result' => [
-				[
-					'message' => $message,
-					'line' => $line,
-				],
-			],
-			'version' => \Jean85\PrettyVersions::getVersion('phpstan/phpstan')->getPrettyVersion(),
-		];
-	}
-
-	$configFiles = [];
+	$configFiles = [
+		'phar://' . $rootDir . '/vendor/phpstan/phpstan/phpstan.phar/conf/staticReflection.neon',
+	];
 	foreach ([
 		'strictRules' => $rootDir . '/vendor/phpstan/phpstan-strict-rules/rules.neon',
 		'bleedingEdge' => 'phar://' . $rootDir . '/vendor/phpstan/phpstan/phpstan.phar/conf/bleedingEdge.neon',
@@ -83,8 +41,6 @@ lambda(function (array $event) {
 	]);
 	file_put_contents($finalConfigFile, $neon);
 
-	require $autoloadPath;
-
 	$containerFactory = new \PHPStan\DependencyInjection\ContainerFactory('/tmp');
 	$container = $containerFactory->create('/tmp', [sprintf('%s/config.level%s.neon', $containerFactory->getConfigDirectory(), $level), $finalConfigFile], [$codePath]);
 
@@ -93,8 +49,8 @@ lambda(function (array $event) {
 
 	/** @var \PHPStan\Analyser\NodeScopeResolver $nodeScopeResolver */
 	$nodeScopeResolver = $container->getByType(\PHPStan\Analyser\NodeScopeResolver::class);
-	$results = $analyser->analyse([$codePath], true, function (string $file) use ($nodeScopeResolver, $codePath, $autoloadPath): void {
-		$nodeScopeResolver->setAnalysedFiles([$codePath, $autoloadPath]);
+	$results = $analyser->analyse([$codePath], true, function (string $file) use ($nodeScopeResolver, $codePath): void {
+		$nodeScopeResolver->setAnalysedFiles([$codePath]);
 	}, null, false);
 
 	$errors = [];
