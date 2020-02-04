d173f7e90a2b0976ae30ce5ab4f6a46d0a0a73b9
Ondrej Mirtes <ondrej@mirtes.cz>
2020-02-04 13:10:12 +0100

    Fix build

--- e2e/test-extension.sh
+++ e2e/test-extension.sh
@@ -9,4 +9,5 @@ cd extension
 composer install
 cp ../phpstan.phar vendor/phpstan/phpstan/phpstan.phar
 cp ../phpstan.phar vendor/phpstan/phpstan/phpstan
+cp ../bootstrap.php vendor/phpstan/phpstan/bootstrap.php
 vendor/bin/phing
