57302e923fb134153f6f4bacdc1419657c22a835
Ondrej Mirtes <ondrej@mirtes.cz>
2020-04-04 15:20:07 +0200

    Website - fix PurgeCSS

--- website/postcss.config.js
+++ website/postcss.config.js
@@ -13,7 +13,7 @@ if (!IS_DEVELOPMENT) {
 
 	plugins.push(
 		purgecss({
-			content: [__dirname + '/*.html'],
+			content: [__dirname + '/*.html', __dirname + '/user-guide/*.html'],
 			defaultExtractor: content => content.match(/[\w-/.:]+(?<!:)/g) || [],
 			whitelistPatterns: [/^hljs/],
 		})
