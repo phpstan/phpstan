c42d042afd91f9d4ce1ba9a81b8202668e39abe7
Ondrej Mirtes <ondrej@mirtes.cz>
2020-04-06 15:12:41 +0200

    Website - show sidebar on mobile

--- website/src/js/MainMenuViewModel.ts
+++ website/src/js/MainMenuViewModel.ts
@@ -3,13 +3,37 @@ import * as ko from 'knockout';
 export class MainMenuViewModel {
 
 	mainMenuOpen: KnockoutObservable<boolean>;
+	sidebarOpen: KnockoutObservable<boolean>;
 
 	constructor() {
 		this.mainMenuOpen = ko.observable(false);
+		this.sidebarOpen = ko.observable(false);
 	}
 
 	toggleMainMenu(): void {
 		this.mainMenuOpen(!this.mainMenuOpen());
+		if (this.mainMenuOpen()) {
+			this.sidebarOpen(false);
+		}
+	}
+
+	openSidebar(): void {
+		this.mainMenuOpen(false);
+		this.sidebarOpen(true);
+	}
+
+	closeSidebar(): void {
+		this.sidebarOpen(false);
+	}
+
+	handleSidebarClick(viewModel: MainMenuViewModel, event: any): boolean {
+		const target: HTMLElement = event.target;
+		if (target.tagName !== 'A') {
+			return true;
+		}
+
+		this.sidebarOpen(false);
+		return true;
 	}
 
 }
