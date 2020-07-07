fc728c677fe9beffc6d2e46fc6e724cff5b1fbb5
Ondrej Mirtes <ondrej@mirtes.cz>
2020-07-07 17:26:12 +0200

    Fix build

--- e2e/test-extension.sh
+++ e2e/test-extension.sh
@@ -6,7 +6,13 @@ set -o nounset
 
 git clone $1 extension
 cd extension
-composer install --ignore-platform-reqs
+
+if [[ "$PHP_VERSION" == "8.0" ]]; then
+  composer install --ignore-platform-reqs
+else
+  composer install
+fi;
+
 cp ../phpstan.phar vendor/phpstan/phpstan/phpstan.phar
 cp ../phpstan.phar vendor/phpstan/phpstan/phpstan
 cp ../bootstrap.php vendor/phpstan/phpstan/bootstrap.php
