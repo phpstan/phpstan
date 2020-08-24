811555cb0df05808259806b1010795ae822aa7bf
Ondrej Mirtes <ondrej@mirtes.cz>
2020-08-24 22:04:49 +0200

    Reproduce MongoDB problem

---
+++ e2e/mongodb/test2.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types = 1);
+
+use MongoDB\Driver\Manager;
+use MongoDB\Driver\BulkWrite;
+use MongoDB\Driver\WriteResult;
+
+class MongoDBE2ETest
+{
+	public function test(): WriteResult
+	{
+		// connect
+		$manager = new Manager("mongodb://localhost:27017");
+		return $manager->executeBulkWrite("collection", new BulkWrite(true));
+	}
+}
