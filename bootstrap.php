38174a60ba7cb0f9f996d48d2538457392e37147
Ondrej Mirtes <ondrej@mirtes.cz>
2021-02-24 12:47:42 +0100

    Revert "Bootstrap - autoload Roave\BetterReflection classes"
    This reverts commit daec3a68dca69b09ab4bb5244345fc3c8f480099.

--- bootstrap.php
+++ bootstrap.php
@@ -27,10 +27,7 @@ spl_autoload_register(function (string $class): void {
 	if (strpos($class, 'PHPStan\\PhpDocParser\\') === 0) {
 		return;
 	}
-
-	$isPhpStan = strpos($class, 'PHPStan\\') === 0;
-	$isBetterReflection = strpos($class, 'Roave\\BetterReflection\\') === 0;
-	if (!$isPhpStan && !$isBetterReflection) {
+	if (strpos($class, 'PHPStan\\') !== 0) {
 		return;
 	}
 
@@ -39,17 +36,8 @@ spl_autoload_register(function (string $class): void {
 	}
 
 	$filename = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-
-	if ($isPhpStan) {
-		$filename = substr($filename, strlen('PHPStan\\'));
-		$filepath = 'phar://' . __DIR__ . '/phpstan.phar/src/' . $filename . '.php';
-	} elseif ($isBetterReflection) {
-		$filename = substr($filename, strlen('Roave\\BetterReflection\\'));
-		$filepath = 'phar://' . __DIR__ . '/phpstan.phar/vendor/ondrejmirtes/better-reflection/src/' . $filename . '.php';
-	} else {
-		return;
-	}
-
+	$filename = substr($filename, strlen('PHPStan\\'));
+	$filepath = 'phar://' . __DIR__ . '/phpstan.phar/src/' . $filename . '.php';
 	if (!file_exists($filepath)) {
 		return;
 	}
