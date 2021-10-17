c510a97e34e3e1148318ff678e5f3f4f9c29447b
Ondrej Mirtes <ondrej@mirtes.cz>
2021-10-17 13:28:13 +0200

    Upgrade Tailwind

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
 	variants: {},
 	plugins: [
 		require('@tailwindcss/ui'),
-		require('@tailwindcss/custom-forms'),
+		require('@tailwindcss/forms'),
 	],
 	purge: {
 		enabled: true,
