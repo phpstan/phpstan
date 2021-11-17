1688deca686b704cfa00a21cc566cf3b7318746e
Ondrej Mirtes <ondrej@mirtes.cz>
2021-11-17 22:19:39 +0100

    Improve integration test

--- e2e/polyfills/test.php
+++ e2e/polyfills/test.php
@@ -5,3 +5,13 @@ var_dump(str_contains('test', 't'));
 function (\Stringable $s): void {
 	echo (string) $s;
 };
+
+class Foo implements \Stringable
+{
+
+	public function __toString()
+	{
+		return 'foo';
+	}
+
+}
