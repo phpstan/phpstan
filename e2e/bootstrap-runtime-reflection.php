5c23ff233fc19dfab6c7094b7a2dcd02c06ce8e6
Ondrej Mirtes <ondrej@mirtes.cz>
2022-04-28 15:49:24 +0200

    Do not test with runtime reflection

--- e2e/bootstrap-runtime-reflection.php
+++
@@ -1,5 +0,0 @@
-<?php declare(strict_types = 1);
-
-require_once __DIR__ . '/../vendor/autoload.php';
-
-\PHPStan\Testing\PHPStanTestCase::$useStaticReflectionProvider = false;
--- e2e/test-extension.sh
+++ e2e/test-extension.sh
@@ -25,10 +25,5 @@ cp ../phpstan.phar vendor/phpstan/phpstan/phpstan.phar
 cp ../phpstan vendor/phpstan/phpstan/phpstan
 cp ../bootstrap.php vendor/phpstan/phpstan/bootstrap.php
 
-if [[ "$RUNTIME_REFLECTION" == "true" ]]; then
-  echo "Running with static reflection"
-  cp ../e2e/bootstrap-runtime-reflection.php tests/bootstrap.php
-fi
-
 make tests
 make phpstan
