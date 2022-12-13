731ab717aeb70bed5528f17aca37fb3e2f07d63b
Ondrej Mirtes <ondrej@mirtes.cz>
2022-12-13 10:15:39 +0100

    phpunit-bridge regression test

---
+++ e2e/phpunit-bridge/src/MyTest.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types = 1);
+
+namespace PhpunitBridgeApp;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class MyTest extends KernelTestCase
+{
+
+	public function testSomething(): void
+	{
+		$this->assertTrue(true);
+	}
+
+}
