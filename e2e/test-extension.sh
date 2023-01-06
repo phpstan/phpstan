525805c171eac56b5c4e5c474d9bbe18dc22e0f5
Ondrej Mirtes <ondrej@mirtes.cz>
2023-01-06 16:00:07 +0100

    Fix phpstan-doctrine testing

--- e2e/test-extension.sh
+++ e2e/test-extension.sh
@@ -10,8 +10,6 @@ cd extension
 if [[ "$PHP_VERSION" == "7.1" || "$PHP_VERSION" == "7.2" ]]; then
   if [[ "$1" == "phpstan-mockery" ]]; then
     composer require --dev phpunit/phpunit:'^7.5.20' mockery/mockery:^1.3 --update-with-dependencies
-  elif [[ "$1" == "phpstan-doctrine" ]]; then
-    composer require --dev phpunit/phpunit:'^7.5.20' doctrine/orm:^2.7.5 doctrine/lexer:^1.0 --no-update --update-with-dependencies
   else
     composer require --dev phpunit/phpunit:'^7.5.20' --update-with-dependencies
   fi;
