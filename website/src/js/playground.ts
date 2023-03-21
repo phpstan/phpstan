8f6bbc82d52c5731c979807933ee92d84fa69db2
Ondrej Mirtes <ondrej@mirtes.cz>
2023-03-21 14:22:09 +0100

    Revert "Update to CodeMirror 6"

--- website/src/js/playground.ts
+++ website/src/js/playground.ts
@@ -1,6 +1,10 @@
 import $ from 'jquery';
 import * as ko from 'knockout';
 import {PlaygroundViewModel} from './PlaygroundViewModel';
+import 'codemirror/addon/edit/matchbrackets';
+import 'codemirror/mode/xml/xml';
+import 'codemirror/mode/clike/clike';
+import 'codemirror/mode/php/php';
 import './handlers';
 
 $(() => {
--- website/src/js/untyped.d.ts
+++ website/src/js/untyped.d.ts
@@ -1,1 +1,3 @@
+declare module "codemirror";
+
 declare module 'docsearch.js/dist/cdn/docsearch.min.js';
