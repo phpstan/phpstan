b7dafd71713af3589db20af6346337506dd0337d
Ondrej Mirtes <ondrej@mirtes.cz>
2023-03-22 17:36:35 +0100

    Improve theme

--- website/src/js/ttcn-theme.ts
+++ website/src/js/ttcn-theme.ts
@@ -1,6 +1,10 @@
 import {HighlightStyle, syntaxHighlighting} from '@codemirror/language'
 import {tags as t} from '@lezer/highlight'
 
+// helpful sources:
+// https://github.com/lezer-parser/php/blob/96115e48b82f1ac6f0e849ff032645b812bd064b/src/highlight.js
+// https://github.com/lezer-parser/php/blob/main/src/php.grammar
+
 export const ttcnHighlightStyle = HighlightStyle.define([
 	{tag: t.quote,
 		color: '#090'},
@@ -27,7 +31,7 @@ export const ttcnHighlightStyle = HighlightStyle.define([
 		color: '#00c'},
 	{tag: t.comment,
 		color: '#333'},
-	{tag: [t.definition(t.variableName), t.className],
+	{tag: [t.definition(t.variableName), t.className, t.name],
 		color: '#00f'},
 	{tag: t.invalid,
 		color: '#f00'},
@@ -41,7 +45,7 @@ export const ttcnHighlightStyle = HighlightStyle.define([
 		color: '#f50'},
 	{tag: t.tagName,
 		color: '#170'},
-	{tag: t.variableName,
+	{tag: [t.variableName, t.propertyName, t.special(t.propertyName)],
 		color: '#05a'},
 	{tag: [t.function(t.variableName), t.special(t.variableName), t.local(t.variableName), t.typeName],
 		color: '#8b2252'},
