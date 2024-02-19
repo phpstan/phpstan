d6be79d175aed0a5ecea350f7d3ba0a4ceb21379
Ondrej Mirtes <ondrej@mirtes.cz>
2024-02-19 10:39:30 +0100

    Website - send release to Sentry

--- website/src/js/playground.ts
+++ website/src/js/playground.ts
@@ -3,9 +3,13 @@ import * as ko from 'knockout';
 import {PlaygroundViewModel} from './PlaygroundViewModel';
 import './codeMirror';
 import * as Sentry from '@sentry/browser';
+import fs from 'fs';
+
+const release = fs.readFileSync(__dirname + '/../release.txt', 'utf8');
 
 Sentry.init({
 	dsn: 'https://52d55b7d270244d99543220b548fed80@o190387.ingest.sentry.io/4505197959184384',
+	release: release
 });
 
 $(() => {
