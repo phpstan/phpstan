8a45f0a3957d873336e331351d3f17b9fb05a175
Markus Staab <markus.staab@redaxo.de>
2024-03-01 13:02:13 +0100

    try FILTER_SANITIZE_ADD_SLASHES

--- e2e/bug10483/bootstrap.php
+++ e2e/bug10483/bootstrap.php
@@ -2,4 +2,4 @@
 
 // constant that's used in the Filter extension that was introduced in a later version of PHP.
 // on earlier php version introduce the same constant via a bootstrap file but with a wrong type
-if(!defined("FILTER_VALIDATE_BOOL"))define("FILTER_VALIDATE_BOOL",false);
+if(!defined("FILTER_SANITIZE_ADD_SLASHES"))define("FILTER_SANITIZE_ADD_SLASHES",false);
--- e2e/bug10483/src/bug10483.php
+++ e2e/bug10483/src/bug10483.php
@@ -2,6 +2,6 @@
 
 function doFoo(mixed $filter): void {
     $x = filter_input_array(INPUT_GET, [
-        'int' => FILTER_VALIDATE_BOOL
+        'int' => FILTER_SANITIZE_ADD_SLASHES
     ], true);
 }
