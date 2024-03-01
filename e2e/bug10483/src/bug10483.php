0e4cc9f2b798dd51a3bb437b5314d66a423aade9
Markus Staab <markus.staab@redaxo.de>
2024-03-01 13:07:33 +0100

    revert

--- e2e/bug10483/src/bug10483.php
+++ e2e/bug10483/src/bug10483.php
@@ -1,7 +1,5 @@
 <?php
 
 function doFoo(mixed $filter): void {
-    $x = filter_input_array(INPUT_GET, [
-        'int' => FILTER_SANITIZE_ADD_SLASHES
-    ], true);
+    var_dump(filter_var("no", FILTER_VALIDATE_BOOL));
 }
