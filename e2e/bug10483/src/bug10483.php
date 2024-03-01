9505305ddde17e383b895de34f80e154e2a564f2
Markus Staab <markus.staab@redaxo.de>
2024-03-01 12:08:04 +0100

    Update bug10483.php

--- e2e/bug10483/src/bug10483.php
+++ e2e/bug10483/src/bug10483.php
@@ -1,6 +1,6 @@
 <?php
 
-function doFoo($filter) {
+function doFoo(mixed $filter): void {
     $x = filter_input_array(INPUT_GET, [
         'int' => FILTER_VALIDATE_INT,
         'positive_int' => $filter,
