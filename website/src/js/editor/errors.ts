87ebe3cc1db1ded2eba53624b8d7a4114a4ecf2d
Ondrej Mirtes <ondrej@mirtes.cz>
2024-06-28 11:07:26 +0200

    Better contrast in dark mode

--- website/src/js/editor/errors.ts
+++ website/src/js/editor/errors.ts
@@ -13,8 +13,8 @@ export const errorsCompartment = new Compartment();
 export const updateErrorsEffect: StateEffectType<boolean> = StateEffect.define();
 
 const buildErrorLines = (doc: Text, errors: PHPStanError[]) => {
-	const errorLineDecoration = Decoration.line({class: 'bg-red-200/50'});
-	const tipLineDecoration = Decoration.line({class: 'bg-yellow-200/50'});
+	const errorLineDecoration = Decoration.line({class: 'bg-red-200/50 dark:bg-rose-500/50'});
+	const tipLineDecoration = Decoration.line({class: 'bg-yellow-200/50 dark:bg-amber-600/50'});
 	const builder = new RangeSetBuilder<Decoration>();
 	const errorLines: number[] = [];
 	const tipLines: number[] = [];
