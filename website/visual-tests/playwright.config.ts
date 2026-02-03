a75d25f4c6eb4b34afd12eece875a0539a6b96ee
Ondrej Mirtes <ondrej@mirtes.cz>
2026-02-03 13:44:18 +0100

    Update visual regression tests workflow

--- website/visual-tests/playwright.config.ts
+++ website/visual-tests/playwright.config.ts
@@ -2,13 +2,13 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
 	testDir: '.',
+	outputDir: './test-results',
 	snapshotDir: './__screenshots__',
 	snapshotPathTemplate: '{snapshotDir}/{testName}/{projectName}/{arg}{ext}',
 	fullyParallel: true,
 	forbidOnly: !!process.env.CI,
 	retries: process.env.CI ? 2 : 0,
-	workers: process.env.CI ? 1 : undefined,
-	reporter: process.env.CI ? 'github' : 'html',
+	reporter: 'html',
 
 	use: {
 		baseURL: 'http://localhost:3000',
