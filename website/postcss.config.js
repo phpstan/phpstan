458b0cf14bc61bc143f0e203f775558fd32c3a65
Ondrej Mirtes <ondrej@mirtes.cz>
2026-02-03 14:39:29 +0100

    Update to TailwindCSS v4

--- website/postcss.config.js
+++ website/postcss.config.js
@@ -1,9 +1,5 @@
-const tailwindcss = require('tailwindcss');
-const autoprefixer = require('autoprefixer');
-
-const plugins = [];
-plugins.push(tailwindcss)
-plugins.push(tailwindcss(__dirname + '/tailwind.config.js'))
-plugins.push(autoprefixer)
-
-module.exports = { plugins }
+module.exports = {
+	plugins: {
+		'@tailwindcss/postcss': {},
+	}
+}
