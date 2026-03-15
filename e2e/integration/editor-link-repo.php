8093dd5389798a55f0281e6a97a9135518715095
Markus Staab <maggus.staab@googlemail.com>
2026-03-15 16:08:58 +0100

    Update editor-link-repo.php

--- e2e/integration/editor-link-repo.php
+++ e2e/integration/editor-link-repo.php
@@ -6,5 +6,6 @@ $commit = shell_exec('git rev-parse HEAD');
 $repoUrl = rtrim(preg_replace('/\.git$/', '', $gitUrl));
 $config = [];
 $config['parameters']['editorUrl'] = sprintf('%s/commit/%s', $repoUrl, $commit);
+$config['parameters']['editorUrlTitle'] = sprintf('%s/commit/%s', $repoUrl, $commit);
 
 return $config;
