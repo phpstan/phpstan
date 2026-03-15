d5c01ec408286cdc96599d3e294687e86f10fc0b
Markus Staab <maggus.staab@googlemail.com>
2026-03-15 16:04:29 +0100

    test linked repo

---
+++ e2e/integration/editor-link-repo.php
@@ -0,0 +1,10 @@
+<?php
+
+$gitUrl = shell_exec('git remote get-url origin');
+$commit = shell_exec('git rev-parse HEAD');
+
+$repoUrl = rtrim(preg_replace('/\.git$/', '', $gitUrl));
+$config = [];
+$config['parameters']['editorUrl'] = sprintf('%s/commit/%s', $repoUrl, $commit);
+
+return $config;
