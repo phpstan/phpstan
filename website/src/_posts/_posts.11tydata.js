74d883595b899ffe1bdf279305762eba74e9a4ce
Ondrej Mirtes <ondrej@mirtes.cz>
2026-03-24 15:41:43 +0100

    Revert "Freeze blog list page for visual screenshots"
    This reverts commit d8a4c0b1b42548682f68f2b408e516865f9e8bf1.

--- website/src/_posts/_posts.11tydata.js
+++ website/src/_posts/_posts.11tydata.js
@@ -1,25 +1,6 @@
 const blogData = require('./../blog.json');
+blogData.layout = "_blogPost.njk";
+blogData.permalink = '/blog/{{ page.fileSlug }}.html';
+blogData.tags = ["blog"];
 
-const freezeBlogPosts = !!process.env.FREEZE_BLOG_POSTS;
-const cutoffYear = 2026;
-
-function isFuturePost(data) {
-	return freezeBlogPosts && data.date instanceof Date && data.date.getFullYear() >= cutoffYear;
-}
-
-module.exports = {
-	...blogData,
-	layout: "_blogPost.njk",
-	tags: ["blog"],
-	eleventyComputed: {
-		permalink: (data) => {
-			if (isFuturePost(data)) {
-				return false;
-			}
-			return `/blog/${data.page.fileSlug}.html`;
-		},
-		eleventyExcludeFromCollections: (data) => {
-			return isFuturePost(data);
-		},
-	},
-};
+module.exports = blogData;
--- website/visual-tests/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Build the site with blog posts from 2026+ excluded for stable visual tests
-FREEZE_BLOG_POSTS=1 npm run build
-
-# Run visual tests, passing through any extra arguments (e.g. --update-snapshots)
-playwright test --config=visual-tests/playwright.config.ts "$@"
