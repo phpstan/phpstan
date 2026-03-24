d8a4c0b1b42548682f68f2b408e516865f9e8bf1
Ondrej Mirtes <ondrej@mirtes.cz>
2026-03-24 14:42:13 +0100

    Freeze blog list page for visual screenshots

--- website/src/_posts/_posts.11tydata.js
+++ website/src/_posts/_posts.11tydata.js
@@ -1,6 +1,25 @@
 const blogData = require('./../blog.json');
-blogData.layout = "_blogPost.njk";
-blogData.permalink = '/blog/{{ page.fileSlug }}.html';
-blogData.tags = ["blog"];
 
-module.exports = blogData;
+const freezeBlogPosts = !!process.env.FREEZE_BLOG_POSTS;
+const cutoffYear = 2026;
+
+function isFuturePost(data) {
+	return freezeBlogPosts && data.date instanceof Date && data.date.getFullYear() >= cutoffYear;
+}
+
+module.exports = {
+	...blogData,
+	layout: "_blogPost.njk",
+	tags: ["blog"],
+	eleventyComputed: {
+		permalink: (data) => {
+			if (isFuturePost(data)) {
+				return false;
+			}
+			return `/blog/${data.page.fileSlug}.html`;
+		},
+		eleventyExcludeFromCollections: (data) => {
+			return isFuturePost(data);
+		},
+	},
+};
